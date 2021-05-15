458e4c761b0cba7a9a0a17d2791fa33466a923be
krishpranav <krisna.pranav@gmail.com>
2021-05-15 14:37:38 +0530

    xspear spec

---
+++ spec/xspear_spec.rb
@@ -0,0 +1,11 @@
+RSpec.describe xspear do
+    it "has a version number" do
+      expect(xspear::VERSION).not_to be nil
+    end
+  
+    it "does something useful" do
+      expect(false).to eq(true)
+    end
+end
+
+  
